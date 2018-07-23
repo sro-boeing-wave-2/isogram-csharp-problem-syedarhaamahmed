e53533653d6489a43f21503b352d3abbb70a4bc9
Syed Arhaam Ahmed <syed.a.ahmed3@boeing.com>
2018-07-23 10:02:34 +0530

    Second Commit with updated code

--- Isogram.Tests/UnitTest1.cs
+++ Isogram.Tests/UnitTest1.cs
@@ -6,9 +6,51 @@ namespace Isogram.Tests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void EmptyStringTest()
         {
+            Assert.True(Class1.IsoGram(""));
+        }
 
+        [Fact]
+        public void LowerCaseIsogramTest()
+        {
+            Assert.True(Class1.IsoGram("isogram"));
+        }
+
+        [Fact]
+        public void ElevenTest()
+        {
+            Assert.False(Class1.IsoGram("eleven"));
+        }
+
+        [Fact]
+        public void LongEnglishWordTest()
+        {
+            Assert.True(Class1.IsoGram("subdermatoglyphic"));
+        }
+
+        [Fact]
+        public void DuplicatedMixedCaseTest()
+        {
+            Assert.True(Class1.IsoGram("Alphabet"));
+        }
+
+        [Fact]
+        public void HyphenWordTest()
+        {
+            Assert.True(Class1.IsoGram("thumbscrew-japingly"));
+        }
+
+        [Fact]
+        public void DuplicatedHyphenCaseTest()
+        {
+            Assert.True(Class1.IsoGram("six-year-old"));
+        }
+
+        [Fact]
+        public void SpaceTest()
+        {
+            Assert.True(Class1.IsoGram("abc xyz"));
         }
     }
 }
--- Isogram/Class1.cs
+++ Isogram/Class1.cs
@@ -1,8 +1,33 @@
 ﻿using System;
+using System.Linq;
 
 namespace Isogram
 {
     public class Class1
     {
+        public static bool IsoGram(string inputStr)
+        {
+            string store = "";
+            if (inputStr == "")
+            {
+                return true;
+            }
+            else
+            {
+                foreach (char c in inputStr)
+                {
+                    if (char.IsLetter(c))
+                        store += c;
+                }
+            }
+            var inputValueActualLength = store.Distinct().Count();
+            var inputValueExpectedlength = store.Count();
+            if (inputValueActualLength == inputValueExpectedlength)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
     }
 }
